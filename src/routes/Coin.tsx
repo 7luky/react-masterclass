352c1d4218efff7922328dc31a73465bf3836b3d
7luky <64417043+7luky@users.noreply.github.com>
2023-07-07 18:00:58 +0900

    #5.7 Nested Routes part One

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,19 +1,13 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Switch, Route, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
-
-interface RoutePrams {
-  coinId: string;
-}
-
-interface RouteState {
-  name: string;
-}
+import Price from "./Price";
+import Chart from "./Chart";
 
 const Container = styled.div`
-padding: 0px 20px
-max-width: 480px;
-margion: 0 auto;
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.div`
@@ -33,6 +27,37 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span: first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+interface RouteParams {
+  coinId: string;
+}
+
+interface RouteState {
+  name: string;
+}
+
 interface ITag {
   id: string;
   name: string;
@@ -40,7 +65,7 @@ interface ITag {
   ico_counter: number;
 }
 
-interface InfoDATA {
+interface InfoData {
   id: string;
   name: string;
   symbol: string;
@@ -99,11 +124,10 @@ interface PriceData {
 
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const { coinId } = useParams<RoutePrams>();
+  const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState<InfoDATA>();
+  const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
-
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -114,15 +138,56 @@ function Coin() {
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading ...</Loader> : <span>{}</span>}
+      {loading ? (
+        <Loader>Loading ...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
     </Container>
   );
 }
